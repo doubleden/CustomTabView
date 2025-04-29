67aededc33a10e8e71c1370cf44243a504a73519
Denis Denisov <denis.denisov8701@gmail.com>
2025-04-29 22:37:03 +0200

    Initial Commit

---
+++ CustomTabView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTabView
+//
+//  Created by Denis Denisov on 29/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTabView/CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabViewApp.swift
+//  CustomTabView
+//
+//  Created by Denis Denisov on 29/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
